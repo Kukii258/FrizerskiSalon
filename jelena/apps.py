51025c1ee6db06eb49f086de46c4e805fe5ae869
Kukii <kukelj.luka@gmail.com>
2025-01-17 11:15:25 +0100

    Initial commit

---
+++ jelena/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JelenaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jelena"
---
+++ salon_jelena/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from salon_jelena.users.models import User
+from salon_jelena.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
