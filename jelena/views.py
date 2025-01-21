ab9a80fb406af18dd6f781705bebad97b1172822
Kukii <kukelj.luka@gmail.com>
2025-01-21 19:43:43 +0000

    Model obavijesti

--- jelena/views.py
+++ jelena/views.py
@@ -1,6 +1,16 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Obavijest
 
 
 def naslovnica(request):
-    return render(request, "html/index.html")
+
+    obavijest = Obavijest.objects.first()
+
+    context = {'obavijest':obavijest}
+    return render(request, "html/index.html",context)
+
+def obavijest(request,obavijest_id):
+    obavijest = get_object_or_404(Obavijest, pk=obavijest_id)
+    return render(request,"html/obavijest.html",{'obavijest':obavijest})
