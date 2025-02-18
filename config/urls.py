24ee4e102dd558606de5c4c8dd80d7b1dbf0e500
kukelj <kukelj.luka@gmail.com>
2025-02-18 16:32:03 +0100

    FIX

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,6 @@ from jelena.admin import custom_admin_site
 
 urlpatterns = [
 
-    path('admin/', custom_admin_site.urls),
     #Galerije
     path('zenska-galerija/', views.galerija, {'model': models.Zenske_frizure, 'template_name': 'html/zenska_galerija.html'},
          name='zenska_galerija'),
