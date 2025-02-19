cffb7663cb8c1f683e6a7a21807942196d768bbb
kukelj <kukelj.luka@gmail.com>
2025-02-19 16:29:58 +0100

    Update media file settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -178,7 +178,7 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
 MEDIA_ROOT = str(APPS_DIR / "media")
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
-MEDIA_URL = "/media/"
+MEDIA_URL = "/app/media/"
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
