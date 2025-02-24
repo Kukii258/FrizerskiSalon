2ed328fa4e0dc1dc2fd9cf68a7f14e737ae67265
kukelj <kukelj.luka@gmail.com>
2025-02-24 21:19:56 +0100

    fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -47,9 +47,7 @@ SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
 SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
-CSRF_TRUSTED_ORIGINS = [
-    "95.111.252.129"
-]
+
 
 # STATIC & MEDIA
 # ------------------------------------------------------------------------------
